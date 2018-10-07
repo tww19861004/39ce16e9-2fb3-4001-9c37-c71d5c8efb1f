feeab919a94772852843af68379d46dab2438b82
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2018-10-07 21:45:22 +0800

    task

---
+++ Async1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async1
+{
+    //await 不会开启新的线程
+    //当前线程会一直往下走直到遇到真正的Async方法（比如说HttpClient.GetStringAsync），
+    //这个方法的内部会用Task.Run或者Task.Factory.StartNew 去开启线程。也就是如果方法不是.NET为我们提供的Async方法，
+    //我们需要自己创建Task，才会真正的去创建线程。
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"方法:Main；Current Thread Id :{Thread.CurrentThread.ManagedThreadId},是否托管线程池：{Thread.CurrentThread.IsThreadPoolThread}");
+            Test();
+            Console.ReadKey(); 
+        }
+
+        static async Task Test()
+        {
+            var getname = GetName();            
+            Console.WriteLine($"方法:Test；Current Thread Id :{Thread.CurrentThread.ManagedThreadId},是否托管线程池：{Thread.CurrentThread.IsThreadPoolThread}");
+            // 方法打上async关键字，就可以用await调用同样打上async的方法
+            // await 后面的方法将在另外一个线程中执行
+            
+            Console.WriteLine($"Current Thread Id :{Thread.CurrentThread.ManagedThreadId};my name is "+ await getname);
+        }
+
+        static async Task<string> GetName()
+        {
+            Console.WriteLine($"方法:GetName1；Current Thread Id :{Thread.CurrentThread.ManagedThreadId},是否托管线程池：{Thread.CurrentThread.IsThreadPoolThread}");
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine($"方法:GetName2；Current Thread Id :{Thread.CurrentThread.ManagedThreadId},是否托管线程池：{Thread.CurrentThread.IsThreadPoolThread}");
+                return "Jesse";
+            });
+        }
+    }
+}
---
+++ thread1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread1
+{
+    class Program
+    {
+        private static bool _isDone = false;
+        static void Main()
+        {
+            //1.线程之间共享数据的问题
+            new Thread(Done).Start();
+            new Thread(Done).Start();
+
+            //2.其它线程的异常，主线程可以捕获到么？
+            try
+            {
+                new Thread(Go).Start();
+            }
+            catch (Exception ex)
+            {
+                // 其它线程里面的异常，我们这里面是捕获不到的。
+                Console.WriteLine("Exception!");
+            }
+
+            //升级了的Task呢？其它线程的异常，主线程可以捕获到么？
+            try
+            {
+                var task = Task.Run(() => { Go(); });
+                task.Wait();  // 在调用了这句话之后，主线程才能捕获task里面的异常
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception!");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Done()
+        {
+            if (!_isDone)
+            {
+                Console.WriteLine("Done");
+                _isDone = true; // 第二个线程来的时候，就不会再执行了(也不是绝对的，取决于计算机的CPU数量以及当时的运行情况)                
+            }
+        }
+
+        static void Go() { throw null; }
+    }
+}
