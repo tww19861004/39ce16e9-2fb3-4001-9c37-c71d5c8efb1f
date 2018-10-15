c42bb828d059fe88f8ce149a04e346f8356617f3
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2018-10-15 23:10:41 +0800

    死锁

---
+++ ConfigureAwait1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigureAwait1
+{
+    class Program
+    {
+        //https://blog.csdn.net/wpwalter/article/details/79673214
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ deadlock1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deadlock1
+{
+    class Program
+    {
+        public static async Task<JObject> GetJsonAsync(Uri uri)
+        {
+            using (var client = new HttpClient())
+            {
+                var jsonString = await client.GetStringAsync(uri).ConfigureAwait(false);
+                return JObject.Parse(jsonString);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
