6e07b1027d26a1e2047abda89eb3d8ddb72f67c3
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2018-10-20 19:48:23 +0800

    3

---
+++ AsyncLazy1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncLazy1
+{
+    public class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        public AsyncLazy(Func<T> valueFactory) :
+            base(() => Task.Factory.StartNew(valueFactory))
+        { }
+        public AsyncLazy(Func<Task<T>> taskFactory) :
+            base(() => Task.Factory.StartNew(() => taskFactory()).Unwrap())
+        { }
+
+        public TaskAwaiter<T> GetAwaiter() { return Value.GetAwaiter(); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
