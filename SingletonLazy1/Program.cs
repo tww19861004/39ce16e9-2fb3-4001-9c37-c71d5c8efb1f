a05b235f11a0e7591ec0d5ac0a9ffcc674f6fc81
tww24098 <tww24098@ly.com>
2018-10-24 18:30:41 +0800

    3

--- SingletonLazy1/Program.cs
+++ SingletonLazy1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SingletonLazy1
@@ -9,7 +10,7 @@ namespace SingletonLazy1
     public sealed class Singleton
     {
         private static readonly Lazy<Singleton> lazy =
-            new Lazy<Singleton>(() => new Singleton());
+            new Lazy<Singleton>(() => new Singleton(), LazyThreadSafetyMode.PublicationOnly);
 
         public static Singleton Instance { get { return lazy.Value; } }
 
@@ -19,14 +20,43 @@ namespace SingletonLazy1
         }
     }
 
+    public class User
+    {
+        private User()
+        {
+            this.id = Guid.NewGuid().ToString();
+        }
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Singleton<T>
+    {
+        private static Lazy<T> _factoryLazy = new Lazy<T>(
+            () => (T)Activator.CreateInstance(typeof(T)),
+            LazyThreadSafetyMode.ExecutionAndPublication);
+
+        public static T Instance
+        {
+            get
+            {
+                return _factoryLazy.Value;
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Parallel.For(0, 100, n =>
+            Parallel.For(0, 10000, n =>
               {
                   Singleton.Instance.ToString();
               });
+            //Parallel.For(0, 100, n =>
+            //  {
+            //      Singleton<User>.Instance.ToString();
+            //  });
             Console.ReadKey();
         }
     }
