13a144453adf39672807501774f03151c87cda03
tww24098 <tww24098@ly.com>
2018-10-29 13:58:22 +0800

    读取dataset

---
+++ excel1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;" + "Data Source=" + "D:\\MyConfiguration\\tww24098\\Downloads\\tcdatacheck20181029101200.xls" + ";" + "Extended Properties=Excel 8.0;";
+            OleDbConnection conn = new OleDbConnection(strConn);
+            conn.Open();
+            string strExcel = "";
+            OleDbDataAdapter myCommand = null;
+            DataSet ds = null;
+            strExcel = "select * from [sheet0$]";
+            myCommand = new OleDbDataAdapter(strExcel, strConn);
+            ds = new DataSet();
+            myCommand.Fill(ds, "table1");
+        }
+    }
+}
