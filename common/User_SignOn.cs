d6bd06528a542ab4d6153ead070eda4c25c60750
tww24098 <tww24098@ly.com>
2018-10-31 17:33:29 +0800

    OleDbConnection

---
+++ common/User_SignOn.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace common
+{
+    public class User_SignOn
+    {
+        public int id
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Name
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
