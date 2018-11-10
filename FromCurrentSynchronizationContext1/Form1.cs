2a5c7367f36e6dd4ef5fb1e267201cd3cb9d35d6
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2018-11-10 22:49:59 +0800

    3

---
+++ FromCurrentSynchronizationContext1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FromCurrentSynchronizationContext1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            Task.Delay(3000).ContinueWith((task) =>
+            {
+                button1.Enabled = true;
+                //},TaskScheduler.FromCurrentSynchronizationContext());
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
