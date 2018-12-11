4bc22f8344ed8e43dca384eb30ecc3630789d4da
tww24098 <tww24098@ly.com>
2018-12-11 21:04:06 +0800

    3

---
+++ MD5-Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace MD5_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static string GetStrMd5(string ConvertString)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            string t2 = BitConverter.ToString(md5.ComputeHash(UTF8Encoding.Default.GetBytes(ConvertString)));
+            t2 = t2.Replace("-", "");
+            return t2;
+        }
+    }
+}
