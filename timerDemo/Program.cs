2eebc3cf65e95fe9ab94f8452ecd7864e2fa649e
tww24098 <tww24098@ly.com>
2019-03-12 19:29:06 +0800

    1

--- timerDemo/Program.cs
+++ timerDemo/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace timerDemo
@@ -11,15 +10,33 @@ namespace timerDemo
     {
         static void Main(string[] args)
         {
-            var timer = new Timer(state =>
-            {
-                Console.WriteLine(state?.ToString()+"每秒执行一次的定时任务,当前线程Id:{0}", Thread.CurrentThread.ManagedThreadId);
-            }, "timer", 0, 5000);
+            //https://www.cnblogs.com/GreenLeaves/p/10117374.html
+            //回唤起更多的线程参与运算,综上所述每个回调方法线程池会给它分配一个线程,到底会分配多少个线程取决于你定的间隔时间.
+            //var timer = new System.Threading.Timer(state =>
+            //{
+            //    Console.WriteLine($"{state?.ToString()}每秒执行一次的定时任务,当前线程Id={System.Threading.Thread.CurrentThread.ManagedThreadId},IsThreadPoolThread={System.Threading.Thread.CurrentThread.IsThreadPoolThread},IsBackground={System.Threading.Thread.CurrentThread.IsBackground}");
+            //}, "timer", 0, 3000);
 
-            var timer2 = new Timer(state =>
+            //var timer2 = new System.Threading.Timer(state =>
+            //{
+            //    Console.WriteLine($"{state?.ToString()}每秒执行一次的定时任务,当前线程Id={System.Threading.Thread.CurrentThread.ManagedThreadId},IsThreadPoolThread={System.Threading.Thread.CurrentThread.IsThreadPoolThread},IsBackground={System.Threading.Thread.CurrentThread.IsBackground}");
+            //}, "timer2", 0, 3000);
+
+            var timer3 = new System.Timers.Timer()
             {
-                Console.WriteLine(state?.ToString()+"每秒执行一次的定时任务,当前线程Id:{0}", Thread.CurrentThread.ManagedThreadId);
-            }, "timer2", 0, 5000);
+                Interval = 1000 * 6
+            };
+            timer3.Elapsed += (obj, events) =>
+            {
+                try
+                {
+                    Console.WriteLine($"timer3每秒执行一次的定时任务,当前线程Id={System.Threading.Thread.CurrentThread.ManagedThreadId},IsThreadPoolThread={System.Threading.Thread.CurrentThread.IsThreadPoolThread},IsBackground={System.Threading.Thread.CurrentThread.IsBackground}");
+                }
+                catch (Exception ex)
+                {
+
+                }
+            };
             Console.ReadKey();
         }
     }
