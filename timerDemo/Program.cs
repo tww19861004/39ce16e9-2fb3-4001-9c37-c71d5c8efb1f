d0139f4a3f143748a4a089a4bdf5df0c3836fee4
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2019-03-19 22:41:53 +0800

    每天进步一点点

--- timerDemo/Program.cs
+++ timerDemo/Program.cs
@@ -12,10 +12,10 @@ namespace timerDemo
         {
             //https://www.cnblogs.com/GreenLeaves/p/10117374.html
             //回唤起更多的线程参与运算,综上所述每个回调方法线程池会给它分配一个线程,到底会分配多少个线程取决于你定的间隔时间.
-            //var timer = new System.Threading.Timer(state =>
-            //{
-            //    Console.WriteLine($"{state?.ToString()}每秒执行一次的定时任务,当前线程Id={System.Threading.Thread.CurrentThread.ManagedThreadId},IsThreadPoolThread={System.Threading.Thread.CurrentThread.IsThreadPoolThread},IsBackground={System.Threading.Thread.CurrentThread.IsBackground}");
-            //}, "timer", 0, 3000);
+            var timer = new System.Threading.Timer(state =>
+            {
+                Console.WriteLine($"{state?.ToString()}每秒执行一次的定时任务,当前线程Id={System.Threading.Thread.CurrentThread.ManagedThreadId},IsThreadPoolThread={System.Threading.Thread.CurrentThread.IsThreadPoolThread},IsBackground={System.Threading.Thread.CurrentThread.IsBackground}");
+            }, "timer", 0, 3000);
 
             //var timer2 = new System.Threading.Timer(state =>
             //{
@@ -26,7 +26,7 @@ namespace timerDemo
             {
                 Interval = 1000 * 6
             };
-            timer3.Start();
+            //timer3.Start();
             timer3.Elapsed += (obj, events) =>
             {
                 try
