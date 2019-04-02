f5f14cee1e8e8c7d46b88647a37bb707d2672783
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2019-04-02 22:37:49 +0800

    .Ａ、Ｂ、Ｃ、Ｄ、Ｅ五名学生有可能参加计算机竞赛，根据下列条件判断哪些

--- 100质数素数/Program.cs
+++ 100质数素数/Program.cs
@@ -11,14 +11,54 @@ namespace _100质数素数
     {
         static void Main(string[] args)
         {
-            //100以内的质数
-            for(int i=0;i<=100;i++)
-            {
-                if(IsZhiShu(i))
-                {
-                    Console.WriteLine(i+"是质数");
-                }
-            }
+
+            //A、B、C、D、E五名学生有有可能参加计算机竞赛
+            //00100,00101,10111....
+            int a, b, c, d, e;            
+            for (a = 0; a < 2; a++)
+                for (b = 0; b < 2; b++)
+                    for (c = 0; c < 2; c++)
+                        for (d = 0; d < 2; d++)
+                            for (e = 0; e < 2; e++)
+                            {
+                                //Console.WriteLine($"{a}{b}{c}{d}{e}");
+                                //A参加时，B也参加；
+                                if (a==1)
+                                {
+                                    b = 1;
+                                }
+                                //B和C只有一个人参加；
+                                if (b==1 ^c==1)
+                                {
+                                    //Ｃ和Ｄ或者都参加，或者都不参加；
+                                    if (c==d)
+                                    {
+                                        //Ｄ和Ｅ中至少有一个人参加；
+                                        if(d==1 || e==1)
+                                        {
+                                            //如果Ｅ参加，那么Ａ和Ｄ也都参加
+                                            if (e==1 &&(a == 0 || d == 0))
+                                            {
+                                                continue;
+                                            }
+                                            Console.WriteLine($"{a}{b}{c}{d}{e}");
+                                        }
+                                        else
+                                        {
+                                            continue;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        continue;
+                                    }
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                            }
+            
             Console.ReadKey();
         }
 
@@ -27,11 +67,11 @@ namespace _100质数素数
         {
             if (n < 2)
                 return false;
-            for(int i = 2;i<n;i++)
+            for (int i = 2; i < n; i++)
             {
                 //能被其他整数相除，不是质数
-                if (n%i == 0)
-                {                    
+                if (n % i == 0)
+                {
                     return false;
                 }
             }
@@ -46,5 +86,24 @@ namespace _100质数素数
                 return 1;
             else return Foo(i - 1) + Foo(i - 2);
         }
+
+        private static string Reverse(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            string[] strArray = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = strArray.Length / 2;
+            for (int i = 0; i < count; i++)
+            {
+                string temp = strArray[i];
+                strArray[i] = strArray[strArray.Length - i - 1];
+                strArray[strArray.Length - i - 1] = temp;
+            }
+            return String.Join(" ", strArray);
+        }
+
+
     }
 }
--- MD5-Test/Program.cs
+++ MD5-Test/Program.cs
@@ -55,43 +55,6 @@ namespace MD5_Test
             return t2;
         }
 
-        /// <summary>
-        /// 获得无重复随机数组
-        /// </summary>
-        /// <param name="n">上限n</param>
-        /// <returns>返回随机数组</returns>
-        static int[] GetRandom(int n)
-        {
-            //容器A和B
-            int[] arryA = new int[n];
-            int[] arryB = new int[n];
-            //填充容器a
-            for (int i = 0; i < arryA.Length; i++)
-            {
-                arryA[i] = i + 1;
-            }
-            //随机对象
-            Random r = new Random();
-            //最后一个元素的索引 如n=100，end=99
-            int end = n - 1;
-            for (int i = 0; i < n; i++)
-            {
-                //生成随机数 因为随机的是索引 所以从0到100取，end=100
-                //一个大于等于 minValue 且小于 maxValue 的 32 位带符号整数，即：返回的值范围包括 minValue 但不包括 maxValue。 
-                //如果 minValue 等于 maxValue，则返回 minValue
-                //
-                int minValue = 0;
-                int maxValue = end + 1;
-                int ranIndex = r.Next(minValue, maxValue);
-                //把随机数放在容器B中
-                arryB[i] = arryA[ranIndex];
-                //用最后一个元素覆盖取出的元素
-                arryA[ranIndex] = arryA[end];
-                //缩减随机数生成的范围
-                end--;
-            }
-            //返回生成的随机数组
-            return arryB;
-        }
+              
     }
 }
