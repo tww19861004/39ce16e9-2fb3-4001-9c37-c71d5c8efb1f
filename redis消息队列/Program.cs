3c332bacbb7a4fda0ef47fe18bc53d48961eb4da
tww24098 <tww24098@ly.com>
2019-04-10 21:06:59 +0800

    2

---
+++ redis消息队列/Program.cs
@@ -0,0 +1,23 @@
+﻿using RedisHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redis消息队列
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //主要借鉴的方法为ListLeftPop及ListRightPush
+            //RedisStore.RedisCache.KeyDelete("201904102002");
+            //先进先出
+            //一个生产者，多个消费者的情况
+            //Redis的列表是使用双向链表实现的，保存了头尾节点，所以在列表头尾两边插取元素都是非常快的
+            
+        }
+    }
+}
