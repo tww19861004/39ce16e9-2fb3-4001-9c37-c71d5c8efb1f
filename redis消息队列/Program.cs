8ab1375b1a40abb200467381f92603649c36c128
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2019-04-11 23:12:19 +0800

    3

--- redis消息队列/Program.cs
+++ redis消息队列/Program.cs
@@ -17,7 +17,8 @@ namespace redis消息队列
             //先进先出
             //一个生产者，多个消费者的情况
             //Redis的列表是使用双向链表实现的，保存了头尾节点，所以在列表头尾两边插取元素都是非常快的
-            
+
+            //https://www.cnblogs.com/stopfalling/p/5375492.html
         }
     }
 }
