d75593adb4302ec96d6950cbb1ff24699f694bfa
tww24098 <tww24098@ly.com>
2019-04-24 17:06:28 +0800

    1234

--- 二维数组/Program.cs
+++ 二维数组/Program.cs
@@ -6,24 +6,36 @@ using System.Threading.Tasks;
 
 namespace 二维数组
 {
+    struct OTAInfo
+    {
+        public int OTAId { get; set; }
+        public string OTAName { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            string[] OTAGroupList = "2,13,27;11,34;15,3;1,42;17,35".Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
-            int[][] test = new int[OTAGroupList.Length][];
+            string[] OTAGroupList = "2_EAN,13_EAN,27_EAN;11_HOTELBEDS,34_HOTELBEDS;15_AGODA,3_AGODA;1_BOOKING海外,42_BOOKINGCN;17_HOTELSPRO,35_HOTELSPRO".Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            OTAInfo[][] test = new OTAInfo[OTAGroupList.Length][];
             for(int i=0;i < OTAGroupList.Length;i++)
             {
                 string[] temp = OTAGroupList[i].Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                test[i] = temp.Select(r => Convert.ToInt32(r)).ToArray();
+                test[i] = new OTAInfo[temp.Length];
+                for (int j=0;j<temp.Length;j++)
+                {
+                    OTAInfo ota = new OTAInfo();
+                    ota.OTAId = Convert.ToInt32(temp[j].Split('_')[0]);
+                    ota.OTAName = temp[j].Split('_')[1];
+                    test[i][j] = ota;
+                }                
             }
             string test1 = Newtonsoft.Json.JsonConvert.SerializeObject(test);
 
-            int[][] test2 = Newtonsoft.Json.JsonConvert.DeserializeObject<int[][]>(test1);
+            OTAInfo[][] test2 = Newtonsoft.Json.JsonConvert.DeserializeObject<OTAInfo[][]>(test1);
 
             int oatid = 1;
             //找到ota小组
-            int[] find = test2.FirstOrDefault(r => r.Any(x => x == oatid));
+            OTAInfo[] find = test2.FirstOrDefault(r => r.Any(x => x.OTAId == oatid));
 
         }
     }
