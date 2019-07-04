fb009e5bcd1d65cc4b479450ffbe078255399640
tww24098 <tww24098@ly.com>
2019-07-04 16:20:35 +0800

    TurboMQ-Kafka-RabbitMQ-RocketMQ

---
+++ Base1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.cnblogs.com/nov5026/archive/2018/08/22/9518520.html
+            //TurboMQ是系统架构支撑部基于淘宝开源的Rocket MQ开发的消息系统，用于支持消息的可靠收发。
+            //目前支持Java和C#使用客户端接入，其中Java客户端由淘宝开源的客户端修改而来， C#客户端由消息中间件组自己开发，在API上基本与官方的Java版客户端对应。
+
+        }
+    }
+}
