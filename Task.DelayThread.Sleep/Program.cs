e2bc0fcca0d82e693abd9529521691031491a07b
TCLDUSER <TCLDUSER@MININT-N5K1UST>
2020-01-12 20:05:46 +0800

    3

---
+++ Task.DelayThread.Sleep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task.DelayThread.Sleep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Threading.Tasks.Task.Delay(5000);
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("12345");
+
+        }
+    }
+}
