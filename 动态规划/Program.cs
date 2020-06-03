1f66c3b53f8190bbf988d02941a6a52d426f940f
tww24098 <tww24098@ly.com>
2020-06-03 21:53:04 +0800

    堆栈实现回文判断

--- 动态规划/Program.cs
+++ 动态规划/Program.cs
@@ -14,7 +14,7 @@ namespace 动态规划
             while (true)
             {
                 string str = Console.ReadLine();// 阻塞处  
-                Console.WriteLine($"{str}{(IsHuiWen(str) ? "是回文" : "不是回文")}");                
+                Console.WriteLine($"{str}{(IsHuiWen1(str) ? "是回文" : "不是回文")}");                
             }
             Console.ReadKey();
         }
@@ -38,6 +38,38 @@ namespace 动态规划
                     return false;
                 }
             }
+            return true;
+        }
+
+
+        /// <summary>
+        /// 判断回文 用堆栈
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        static bool IsHuiWen1(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return false;
+            }
+            Stack<char> stack = new Stack<char>();
+            Queue<char> queue = new Queue<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                stack.Push(str[i]);
+                queue.Enqueue(str[i]);
+            }
+            
+            while (stack.Count > 0)
+            {
+                if (stack.Pop() != queue.Dequeue())   //只要发现有一个不等，就把isHui设置为假
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
     }
